c3242a42468c9fb1fcd6c38a96b49001643bec6d
lixp <894646171@qq.com>
2020-08-19 17:31:39 +0800

    add config of Mainet and Testnet

--- src/js/globalConfig.js
+++ src/js/globalConfig.js
@@ -5,10 +5,12 @@ export const MAINNET = {
     chainType: ChainType.Harmony,
     chainId: ChainID.HmyMainnet,
     shardID: 0,
+    explorer: "https://explorer.harmony.one",
 }
 export const TESTNET = {
     url: "https://api.s0.b.hmny.io",
     chainType: ChainType.Harmony,
     chainId: ChainID.HmyTestnet,
     shardID: 0,
+    explorer: "https://explorer.testnet.harmony.one",
 }
