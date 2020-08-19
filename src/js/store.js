cbeef0b667648eb1bd44003422fb8ef5967bbbcc
lixp <894646171@qq.com>
2020-08-19 22:39:18 +0800

    adjust default value of gas

--- src/js/store.js
+++ src/js/store.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 
 const store = {
   data: {
-    gasLimit: "0xc3500",
+    gasLimit: "0x7a1200",
     gasPrice: "0x3b9aca00", // 1 Gwei
     callValue: "0x0",
     waitConfirm: true,
