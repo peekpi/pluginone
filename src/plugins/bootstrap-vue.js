6ac345ff9fbc96e9803a5531f4f246568df74ce0
lixp <894646171@qq.com>
2020-08-19 17:30:58 +0800

    modify default config of bootstrap

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -4,4 +4,24 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
+const smallSize = { size: "sm" };
+
+const config = {
+    formControls: smallSize,
+    BForm: smallSize,
+    BFormText: smallSize,
+    BFormGroup: smallSize,
+    BFormSelect: smallSize,
+    BButton: smallSize,
+    BDropdown: smallSize,
+    BInputGroup: smallSize,
+    BInput: smallSize,
+    BModal: smallSize,
+    BPagination: smallSize,
+    BPaginationNav: smallSize,
+};
+Vue.use(BootstrapVue, config)
+
+
+import { IconsPlugin } from 'bootstrap-vue'
+Vue.use(IconsPlugin)
