d8c5fa4d2ff9ee9e1091b9d99d45646bd2878454
lixp <894646171@qq.com>
2020-09-18 00:15:58 +0800

    remove bootstrap css reference

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const smallSize = { size: "sm" };
 
